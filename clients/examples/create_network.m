fa59907d992d7e0a8fbad152fc01e893d410a506
medihack <schlamp@gmx.de>
2014-06-03 09:34:17 +0200

    improved client examples

--- clients/examples/create_network.m
+++ clients/examples/create_network.m
@@ -1,9 +1,8 @@
 addpath(genpath('../matlab/blink'));
 
-network = Network('Subject 1');
-
+% create network object
+network = Network('Example Subject');
 network.matrix_data = loadjson('matrix.json');
-
 regions_data = loadjson('regions.json');
 for k=1:length(regions_data)
     data = regions_data{k};
@@ -15,6 +14,7 @@ for k=1:length(regions_data)
     network = network.addRegion(label, fullName, x, y, z);
 end
 
+% create request object and upload to BLINK server
 token = '2a367de02f52b927935cfa192422a2305eb3a087';
 req = Request(token, true, true);
 networkId = req.create(network);
--- clients/examples/create_network.py
+++ clients/examples/create_network.py
@@ -6,40 +6,37 @@ import os
 folder = os.path.dirname(__file__)
 sys.path.append(os.path.join(folder, "../src/python"))
 
-from blink import blink
+from blink import client
 import json
 
 if __name__ == '__main__':
+    # load matrix data from json file
     matrix_data = None
     matrix_file = os.path.join(folder, "matrix.json")
     with open(matrix_file) as infile:
         matrix_data = json.load(infile)
 
+    # load regions data from json file
     regions_data = None
     regions_file = os.path.join(folder, "regions.json")
     with open(regions_file) as infile:
         regions_data = json.load(infile)
 
-    token = "2a367de02f52b927935cfa192422a2305eb3a087"
-    request = blink.Request(token, dev=True)
-
-    subject = "Subject 1"
-    network = blink.Network(subject)
+    # create network object
+    network = client.Network("Example Subject")
     network.matrix = matrix_data
     network.regions = regions_data
-
     network.project = 'Human Connectome Project'
     network.atlas = 'AAL'
-
-    # subject
-    network.subject_type = blink.SubjectType.single
-    network.gender = blink.Gender.male
+    network.subject_type = client.SubjectType.single
+    network.gender = client.Gender.male
     network.age = 23
-
-    # protocol
     network.scanner_device = 'Siemens Trio'
     network.scanner_parameters = 'TR 720ms, TE 33.1ms, Flip Angle 52°, ST 2.0mm iso, Multiband 8, BW 2290 Hz/Px'
     network.preprocessing = 'Preprocessing pipeline of Connectome Project, Smoothing 5mm Kernel'
 
+    # create request object and upload network to BLINK server
+    token = "2a367de02f52b927935cfa192422a2305eb3a087"
+    request = client.Request(token, dev=True)
     result = request.create(network)
-    print result
+    print "Created example network with ID: " + result
