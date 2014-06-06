fe33b6e803212b102d6d13b2d0d23f5bc5ff679c
medihack <schlamp@gmx.de>
2014-06-06 14:14:43 +0200

    fix retrieve_network

--- clients/examples/retrieve_network.py
+++ clients/examples/retrieve_network.py
@@ -4,11 +4,11 @@
 import sys
 import os
 folder = os.path.dirname(__file__)
-sys.path.append(os.path.join(folder, "../lib/python"))
+sys.path.append(os.path.join(folder, "../src/python"))
 
-from blink import blink
+from blink import client
 
 token = "2a367de02f52b927935cfa192422a2305eb3a087"
-request = blink.Request(token, dev=True)
+request = client.Request(token, dev=True)
 network = request.retrieve(1)
 print network.__dict__
